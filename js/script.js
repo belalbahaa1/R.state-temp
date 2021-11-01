70ce6ddd74b37952fa8f5605648bae9c456d4591
belalbahaa1 <60116768+belalbahaa1@users.noreply.github.com>
2021-11-02 01:59:54 +0200

    upload

---
+++ js/script.js
@@ -0,0 +1,31 @@
+var swiper = new Swiper('#property-slider .swiper', {
+    // Optional parameters
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 30,
+    centeredSlides: true,
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 50,
+    },
+  }
+  
+
+  });
+
+
+  var swiper = new Swiper('#testimonial-slider .swiper', {
+    // Optional parameters
+    loop: true,
+  
+
+  });
